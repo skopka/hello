6d47fb5e9b407e38bbf3321a6c43c708ac8351c6
Aleksey Skopintsev <skopka-1992@rambler.ru>
2025-09-18 14:16:46 +0300

    In work

---
+++ src/Skopka.Hello/Data/AppDbContext.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace Skopka.Hello.Data;
+
+public class AppDbContext : DbContext
+{
+    public AppDbContext(DbContextOptions<AppDbContext> options) : base(options)
+    {
+    }
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        base.OnModelCreating(modelBuilder);
+        // TODO: add entities and configurations (Users, Credentials, Clients, ClientKeys, Consents, Scopes, RefreshTokens, Keys, Audit)
+    }
+}
